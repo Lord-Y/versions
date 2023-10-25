b1a5f710b965c2c6d69593f948d82f9e5ea61f0e
Lord-Y <Lord-Y@users.noreply.github.com>
2023-10-25 10:57:12 +0200

    [ui] - Update tailwindcss config

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,6 +1,7 @@
 const flattenColorPalette =
   require('tailwindcss/lib/util/flattenColorPalette').default
 
+/* eslint-env node */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
