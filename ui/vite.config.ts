1a1d1f8d73e64c1458e5fa13f2ccebb69e5b7cec
Lord-Y <Lord-Y@users.noreply.github.com>
2023-10-25 10:56:06 +0200

    [ui] - Upgrade to last stable version of vite/vue

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,35 +1,31 @@
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
 let api_url: string
 if (process.env.API_GATEWAY) {
   api_url = `${process.env.API_GATEWAY}`
 } else {
-  api_url = 'http://127.0.0.1:8080'
+  api_url = 'http://127.0.0.1:8081'
 }
+const port: number = 3000
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ui/',
-  resolve: {
-    alias: {
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js',
-      '@': path.resolve(__dirname, 'src/'),
-    },
-  },
   plugins: [
     vue(),
-    vueI18n({
-      include: path.resolve(__dirname, './src/i18n/locales/**'),
-    }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: '/ui/',
   preview: {
-    port: 3000,
+    port: port,
   },
   server: {
-    port: 3000,
+    port: port,
     strictPort: true,
     proxy: {
       '/api': {
